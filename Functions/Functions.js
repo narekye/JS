bbd5a69c2e58585e0743504c62f03f63865e42cc
narekye <yegoryan.narek@gmail.com>
2017-03-22 21:32:32 -0600

    :lollipop: object functions

--- Functions/Functions.js
+++ Functions/Functions.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+var user = {
+	name: "Jim";
+	sayhi: function () {
+		alert(this.name);
+	}
+};
+user.sayhi();
+// second example
+var user1 = {
+	name: "Alice";
+};
+user1.hello = function () {
+	alert("hello");
+};
