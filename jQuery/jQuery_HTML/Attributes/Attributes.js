20e7694598ff4772082bea46c90ba18abe907edd
narekye <yegoryan.narek@gmail.com>
2017-03-24 00:58:08 -0600

    :lollipop: attributes via attr()

---
+++ jQuery/jQuery_HTML/Attributes/Attributes.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+$(document).ready(function () {
+	$("button").click(function () {
+		$("#site").attr({
+			"href": "https://www.google.com",
+			"title": "Google Search"
+		});
+	});
+});
