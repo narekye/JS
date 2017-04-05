0a129a3d1a02da72e99b0fad4d8135174454341e
narekye <yegoryan.narek@gmail.com>
2017-04-05 15:11:24 -0600

    :octocat: some checks :octocat:

--- Object_Oriented_Programming/Functional_Style/functional_style.js
+++ Object_Oriented_Programming/Functional_Style/functional_style.js
@@ -7,14 +7,14 @@ function CoffeeMachine(power,capacity) {
 			throw new Error("Amount must be a + value");
 		}
 		if (amount > capacity) {
-			throw new Error("It can't be.")
+			throw new Error("It can't be.");
 		}
 		this.wateramount = amount;
-	}
+	};
 	// getter for wateramount
 	this.getwateramount = function () {
 		return wateramount;
-	}
+	};
 	// getter setter property example. one function
 	this.waterprop = function (amount) {
 		if (!arguments.length) {
@@ -27,7 +27,7 @@ function CoffeeMachine(power,capacity) {
 			throw new Error("Coffeemachine doesnt support this amount");
 		}
 		this.wateramount = amount;
-	}
+	};
 	// some functions
 	function getBoilTime() {
 		return 1000;
@@ -37,7 +37,7 @@ function CoffeeMachine(power,capacity) {
 	}
 	this.run = function () {
 		setTimeout(onReady, getBoilTime());
-	}
+	};
 }
 var coffeemachine = new CoffeeMachine(100,200);
 coffeemachine.run();
--- Object_Oriented_Programming/Functional_Style/inheritance.js
+++ Object_Oriented_Programming/Functional_Style/inheritance.js
@@ -2,17 +2,17 @@ function Machine() {
 	var enabled = false;
 	this.enable = function () {
 		enabled = true;
-	}
+	};
 	this.disable = function () {
 		enabled = false;
-	}
+	};
 }
 function CoffeeMachine(amount) {
 	Machine.call(this);
 	var wateramount = 0;
 	this.setWaterAmount = function (amount) {
 		wateramount = amount;
-	}
+	};
 }
 
 var coffemachine = new CoffeeMachine(200);
