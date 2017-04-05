e7800b21511c7b9e4e2025af6bfb482b3da1107d
narekye <yegoryan.narek@gmail.com>
2017-04-04 23:03:45 -0600

    :octocat: Inheritance :octocat:

---
+++ Object_Oriented_Programming/Functional_Style/inheritance.js
@@ -0,0 +1,21 @@
+function Machine() {
+	var enabled = false;
+	this.enable = function () {
+		enabled = true;
+	}
+	this.disable = function () {
+		enabled = false;
+	}
+}
+function CoffeeMachine(amount) {
+	Machine.call(this);
+	var wateramount = 0;
+	this.setWaterAmount = function (amount) {
+		wateramount = amount;
+	}
+}
+
+var coffemachine = new CoffeeMachine(200);
+coffemachine.enable();
+coffemachine.setWaterAmount = 300;
+coffemachine.disable();
