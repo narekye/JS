1032847c446529a2163c46d245c8225a81df2667
narekye <yegoryan.narek@gmail.com>
2017-04-06 23:24:51 -0600

    :octocat: added F.prototype.functions :octocat:

--- Object_Oriented_Programming/Prototype_Style/classes_on_prototypes.js
+++ Object_Oriented_Programming/Prototype_Style/classes_on_prototypes.js
@@ -1,1 +1,12 @@
+function Animal(name) = {
+	this.name = name;
+	this.speed = 0;
+};
 
+Animal.prototype.run = function (speed) {
+	this.speed += speed;
+	alert("The animal runs " + this.speed + "speed");
+};
+Animal.prototype.stop = function () {
+	this.speed = 0;
+}
