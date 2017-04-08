b7157aebb02d25f5415c08728f1e075fc888e7d0
narekye <yegoryan.narek@gmail.com>
2017-04-07 23:18:42 -0600

    :octocat: made some changes :octocat:

--- Object_Oriented_Programming/Prototype_Style/classes_on_prototypes.js
+++ Object_Oriented_Programming/Prototype_Style/classes_on_prototypes.js
@@ -14,5 +14,5 @@ Animal.prototype.stop = function () {
 var animal = new Animal("Dog");
 alert(animal.speed);
 animal.run(5); // animal runs speed = 5;
-animal.run(5); // animal runs speed = 10;
+animal.run(5); // animal runs speed = 10;  
 animal.stop(); // animal stopped; 
